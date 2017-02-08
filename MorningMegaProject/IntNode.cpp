3a7423e7f681a7b0ab3e355a6f0eeefc77cc0a73
snoupyrelete <drob8896@740s-pl-112.local>
2017-02-08 08:17:46 -0700

    First commit

---
+++ MorningMegaProject/IntNode.cpp
@@ -0,0 +1,49 @@
+//
+//  IntNode.cpp
+//  MorningMegaProject
+//
+//  Created by Robson, Dylan on 2/8/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+#include "IntNode.hpp"
+
+
+IntNode :: IntNode()
+{
+    this->nodeData = -999;
+    this->nodePointer = nullptr;
+}
+
+IntNode :: IntNode(int data)
+{
+    this->nodeData = data;
+    this->nodePointer = nullptr;
+}
+
+IntNode :: IntNode(int nodeData, IntNode * next)
+{
+    this->nodeData = nodeData;
+    this->nodePointer = next;
+}
+
+int IntNode :: getNodeData()
+{
+    return nodeData;
+}
+
+IntNode * IntNode :: getNodePointer()
+{
+    return nodePointer;
+}
+
+void IntNode :: setNodeData(int nodeData)
+{
+    this->nodeData = nodeData;
+}
+
+void IntNode :: setNodePointer(IntNode * next)
+{
+    this->nodePointer = next;
+}
+
---
+++ MorningMegaProject/IntNode.hpp
@@ -0,0 +1,34 @@
+//
+//  IntNode.hpp
+//  MorningMegaProject
+//
+//  Created by Robson, Dylan on 2/8/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+#ifndef IntNode_hpp
+#define IntNode_hpp
+
+class IntNode
+{
+private:
+    int nodeData;
+    IntNode * nodePointer;
+public:
+    // Constructors
+    IntNode();
+    IntNode(int value);
+    /*
+     Creates an IntNode with a specfied value and reference to the next node
+    */
+    IntNode(int value, IntNode * nextNode);
+    
+    // Methods
+    int getNodeData();
+    IntNode * getNodePointer();
+    
+    void setNodeData(int value);
+    void setNodePointer(IntNode * updateNode);
+};
+
+#endif /* IntNode_hpp */
