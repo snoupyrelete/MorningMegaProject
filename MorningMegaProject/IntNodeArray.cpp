35195232b87b7e5796d3b7c896b06cb6b2d8e601
snoupyrelete <drob8896@740s-pl-112.local>
2017-02-08 08:46:42 -0700

    Added header for IntNodeArray

---
+++ MorningMegaProject/IntNodeArray.cpp
@@ -0,0 +1,9 @@
+//
+//  IntNodeArray.cpp
+//  MorningMegaProject
+//
+//  Created by Robson, Dylan on 2/8/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+#include "IntNodeArray.hpp"
---
+++ MorningMegaProject/IntNodeArray.hpp
@@ -0,0 +1,27 @@
+//
+//  IntNodeArray.hpp
+//  MorningMegaProject
+//
+//  Created by Robson, Dylan on 2/8/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+#ifndef IntNodeArray_hpp
+#define IntNodeArray_hpp
+
+#include "IntNode.cpp"
+
+class IntNodeArray
+{
+private:
+    IntNode * head;
+    int size;
+public:
+    IntNodeArray(int bigness);
+    
+    int getSize();
+    int getFromIndex(int value);
+    void setAtIndex(int index, int value);
+};
+
+#endif /* IntNodeArray_hpp */
