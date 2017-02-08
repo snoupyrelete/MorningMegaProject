82271acacf8e14b8bbd6fcf4bbb6b16b9801bc31
snoupyrelete <drob8896@740s-pl-112.local>
2017-02-08 07:58:10 -0700

    Initial Commit

---
+++ MorningMegaProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MorningMegaProject
+//
+//  Created by Robson, Dylan on 2/8/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
