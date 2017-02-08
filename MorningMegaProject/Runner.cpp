bc422f3ca0fbb84a32015eb26633b7f19b17cee6
snoupyrelete <drob8896@740s-pl-112.local>
2017-02-08 09:26:31 -0700

    Header for StructureController

---
+++ MorningMegaProject/Runner.cpp
@@ -0,0 +1,17 @@
+//
+//  Runner.cpp
+//  MorningMegaProject
+//
+//  Created by Robson, Dylan on 2/8/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+#include "StructureController.hpp"
+
+int main()
+{
+    StructureController * app = new StructureController();
+    app->start();
+    
+    return 0;
+}
---
+++ MorningMegaProject/StructureController.cpp
@@ -0,0 +1,20 @@
+//
+//  StructureController.cpp
+//  MorningMegaProject
+//
+//  Created by Robson, Dylan on 2/8/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+#include "StructureController.hpp"
+
+StructureController :: StructureController()
+{
+    
+}
+
+StructureController :: start()
+{
+    
+}
+
