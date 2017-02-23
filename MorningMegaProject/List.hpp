b5334b67642768e966cb0863c0bebc9ce739a1df
snoupyrelete <drob8896@740s-pl-112.local>
2017-02-23 08:36:04 -0700

    created add front method for List

--- MorningMegaProject/List.hpp
+++ MorningMegaProject/List.hpp
@@ -14,14 +14,15 @@
 #include <iostream>
 
 /*----
- HEADER
- ----*/
+HEADER
+----*/
 
 template <class Type>
 class List
 {
 private:
     Node<Type> * front;
+    Node<Type> * end;
     int size;
 public:
     // Constructors
@@ -35,13 +36,16 @@ public:
     
     // Methods
     void addAtIndex(int index, Type value);
-    void add(Type value);
+    void addFront(Type value);
+    void addEnd(Type value);
     Type remove(int index);
     Type setAtIndex(int index, Type value);
     Type getFromIndex(int index);
     bool contains(Type data);
+    
     int getSize() const;
     Node<Type> * getFront() const;
+    Node<Type> * getEnd() const;
 };
 
 /*------------
@@ -51,9 +55,43 @@ IMPLEMENTATION
 template <class Type>
 List<Type> :: List()
 {
-    
+    this->size = 0;
+    this->front = nullptr;
+    this->end = nullptr;
 }
 
+
+template <class Type>
+void List<Type> :: addFront(Type value)
+{
+    if (size == 0)
+    {
+        // DEFAULT CASE - have to set the single node to both front & end pointers.
+        Node<Type> * first = new Node(value)
+        this->front = first;
+        this->end = first;
+    }
+    else
+    {
+        Node<Type> * newFirst = new Node(value)
+        newFirst->setNodeData(value);
+        // Set the new Node to point to the previously first Node
+        newFirst->setNodePointer(front);
+        
+        // Node<Type> * newFirst = new Node<Type>(value, front);
+        
+        // Point the front to the new front.
+        front = newFirst;
+    }
+    
+    size++;
+}
+
+template <class Type>
+void List<Type> :: addFront(Type value)
+{
+    
+}
 
 
 
