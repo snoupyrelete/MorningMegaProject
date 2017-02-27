7e1639293448f764563b654eb4e1ed84a4aeb22f
snoupyrelete <drob8896@740s-pl-112.local>
2017-02-27 09:44:40 -0700

    Bi Directional Node header

---
+++ MorningMegaProject/BiDirectionalNode.hpp
@@ -0,0 +1,35 @@
+//
+//  BiDirectionalNode.hpp
+//  MorningMegaProject
+//
+//  Created by Robson, Dylan on 2/27/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+#ifndef BiDirectionalNode_hpp
+#define BiDirectionalNode_hpp
+
+template <class Type>
+class BiDirectionalNode : public Node<Type>
+{
+private:
+    Type data;
+    BiDirectionalNode<Type> * previousPointer;
+    BiDirectionalNode<Type> * nextPointer;
+public:
+    BiDirectionalNode<Type>();
+    BiDirectionalNode<Type>(Type data);
+    BiDirectionalNode<Type>(Type data, BiDirectionalNode<Type> * prev, BiDirectionalNode<Type> * next);
+
+    Type getNodeData();
+    BiDirectionalNode<Type> * getNextPointer();
+    BiDirectionalNode<Type> * getPreviousPointer();
+    
+    void setNodeData(Type data);
+    void setNextPointer(BiDirectionalNode<Type> * next);
+    void setPreviousPointer(BiDirectionalNode<Type> * prev);
+    
+};
+
+
+#endif /* BiDirectionalNode_hpp */
