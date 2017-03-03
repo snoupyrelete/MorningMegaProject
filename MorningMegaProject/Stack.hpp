58c10b15dc7ea5a921d757eaa2628d743e77bbb5
snoupyrelete <drob8896@740s-pl-112.local>
2017-03-03 09:32:50 -0700

    Did some stuff I guess? idk

--- MorningMegaProject/Stack.hpp
+++ MorningMegaProject/Stack.hpp
@@ -15,7 +15,6 @@ template <class Type>
 class Stack : public DoublyLinkedList<Type>
 {
 private:
-    
 public:
     Stack();
     ~Stack();
@@ -30,10 +29,34 @@ public:
 Add method only adds to the end on a stack. Never at an index 
 */
 template <class Type>
-void Stack<Type> :: add(Type valueToAdd) : DoublyLinkedList<Type> :: add(valueToAdd)
+void Stack<Type> :: add(Type valueToAdd)
 {
-    
+    push(valueToAdd);
 }
 
+/* 
+Push adds the supplied object to the end of the stack.
+Set new object to point to end
+Increases the size by 1.
+Adjusts the end pointer to reflect the new end of the stack.
+
+*/
+template <class Type>
+void Stack<Type> :: push(Type valueToPush)
+{
+    BiDirectionalNode<Type> * newNode = new BiDirectionalNode(valueToPush);
+    if(this->size == 0 || this->front == nullptr || this->end == nullptr)
+    {
+        this->front = newNode;
+    }
+    else
+    {
+        this->end->setNextPointer(newNode);
+        newNode->setPreviousPointer(this->end);
+    }
+
+    this->end = newNode;
+    this->size++;
+}
 
 #endif /* Stack_hpp */
