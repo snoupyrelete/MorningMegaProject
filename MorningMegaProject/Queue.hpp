4c06619a28da1c87a1a0943669da00f1a4ce8b63
snoupyrelete <drob8896@740s-pl-112.local>
2017-03-07 08:42:23 -0700

    Added dequeue method in Queue

--- MorningMegaProject/Queue.hpp
+++ MorningMegaProject/Queue.hpp
@@ -18,8 +18,77 @@ private:
 public:
     Queue();
     void add(Type data);
-
+    Type peek();
+    Type dequeue();
+    Type remove(int index);
+    Type enqueue();
     
 };
+
+
+
+template <class Type>
+Type Queue<Type> :: enqueue()
+{
+    
+}
+
+template <class Type>
+Type Queue<Type> :: remove(int index)
+{
+    assert(index  == 0 && this->getSize() > 0)
+    return dequeue();
+}
+
+/*
+ 1. Assert size is valid
+ 2. Get data from front node
+ 3. If size == 1, set end to nullptr
+ 4. Else, move front to front's next
+ 4a. Set front's previous to nullptr
+ 5. Delete old front node
+ 6. Adjust size --
+ 7. Return old value that was dequeued
+ */
+template <class Type>
+Type Queue<Type> :: dequeue()
+{
+    // 1
+    assert(this->getSize() > 0);
+    // 2
+    Type removedValue = this->getFront()->getNodeData();
+    BiDirectionalNode<Type> * removeMe = this->getFront();
+    // 3
+    if(this->getSize() == 1)
+    {
+        this->setEnd(nullptr);
+        this->setFront(nullptr);
+    }
+    // 4
+    else
+    {
+        this->setFront(removeMe->getNextPointer());
+    }
+    this->setFront()->setPreviousPointer(nullptr);
+    // 5
+    delete removeMe;
+    //6
+    this->setSize(this->getSize() - 1);
+    // 7
+    return removedValue;
+}
+
+/*
+ 1. Check that size is greather than 0
+ 2. Return the front object's data
+ */
+template <class Type>
+Type Queue<Type> :: peek()
+{
+    assert(this->getSize() > 0);
+    
+    return this->getFront()->getNodeData();
+}
+
 
 #endif /* Queue_hpp */
