8215b5a21d2687516ac2fa8f3fed342334bdc858
snoupyrelete <drob8896@740s-pl-112.local>
2017-04-12 07:23:36 -0600

    BinarySearchTree

--- MorningMegaProject/BinarySearchTree.hpp
+++ MorningMegaProject/BinarySearchTree.hpp
@@ -34,4 +34,64 @@ public:
     void postOrderTraversal();
 };
 
+teemplate <class Type>
+BinarySearchTree<Type> :: BinarySearchTree() : Tree<Type>()
+{
+    this->root = nullptr;
+}
+
+template <class Type>
+BinarySearchTreeNode<Type>* BinarySearchTree<Type> :: getRoot()
+{
+    return this->root;
+}
+
+template <class Type>
+void BinarySearchTree<Type> :: setRoot(BinarySearchTreeNode<Type> * root)
+{
+    this->root = root;
+}
+
+template <class Type>
+void BinarySearchTree<Type> :: inOrderTraversal()
+{
+    
+}
+
+template <class Type>
+void BinarySearchTree<Type> :: preOrderTraversal()
+{
+    
+}
+
+template <class Type>
+void BinarySearchTree<Type> :: postOrderTraversal()
+{
+    
+}
+
+template <class Type>
+int BinarySearchTree<Type> :: calculateSize(BinarySearchTreeNode<Type> * start)
+{
+    return -99;
+}
+
+template <class Type>
+void BinarySearchTree<Type> :: inOrderTraversal(BinarySearchTreeNode<Type> * inStart)
+{
+    
+}
+
+template <class Type>
+void BinarySearchTree<Type> :: preOrderTraversal(BinarySearchTreeNode<Type> * preStart)
+{
+    
+}
+
+template <class Type>
+void BinarySearchTree<Type> :: postOrderTraversal(BinarySearchTreeNode<Type> * postStart)
+{
+    
+}
+
 #endif /* BinarySearchTree_hpp */
