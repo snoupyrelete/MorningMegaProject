40891bed97eb1fdec1ab9db48a61330858a49d2b
snoupyrelete <drob8896@740s-pl-112.local>
2017-04-25 09:52:42 -0600

    Fixed error

--- MorningMegaProject/AVLTree.hpp
+++ MorningMegaProject/AVLTree.hpp
@@ -125,7 +125,7 @@ void AVLTree<Type> :: remove(Type value)
 }
 
 template <class Type>
-void AVLTree<Type> :: insert(Type value)
+void AVLTree<Type> :: insert(Type item)
 {
     insertNode(this->getRoot(), item);
 }
@@ -144,5 +144,59 @@ BinarySearchTreeNode<Type> * AVLTree<Type> :: insertNode(BinarySearchTreeNode<Ty
         parent = balanceSubTree(parent);
     }
     return parent;
+}
+
+template <class Type>
+AVLTree<Type> :: ~AVLTree()
+{
+    delete this->getRoot();
+}
+
+
+template <class Type>
+BinarySearchTreeNode<Type> * AVLTree<Type> :: removeNode(BinarySearchTreeNode<Type> * parent, Type inserted)
+{
+    if(parent == nullptr)
+    {
+        return parent;
+    }
+    if(inserted < parent->getNodeData())
+    {
+        parent->setLeftChild(removeNode(parent->getLeftChild(), inserted));
+    }
+    else if(inserted > parent->getNodeData())
+    {
+        parent->setRightChild(removeNode(parent->getRightChild(), inserted));
+    }
+    else
+    {
+        BinarySearchTreeNode<Type> * temp;
+        if(parent->getLeftChild() == nullptr && parent->getRightChild() == nullptr)
+        {
+            temp = parent;
+            delete temp;
+        }
+        else if(parent->getLeftChild() == nullptr)
+        {
+            *parent = *parent->getRightChild();
+        }
+        else if(parent->getRightChild() == nullptr)
+        {
+            *parent = *parent->getLeftChild();
+        }
+        else
+        {
+            BinarySearchTreeNode<Type> * leftMost = this->getLeftMostChild(parent->getRightChild());
+            parent->setNodeData(leftMost->getNodeData());
+            parent->setRightChild(removeNode(parent->getRightChild(), leftMost->getNodeData()));
+        }
+    }
+    
+    if(parent == nullptr)
+    {
+        return parent;
+    }
+    
+    return balanceSubTree(parent);
 }
 #endif /* AVLTree_hpp */
